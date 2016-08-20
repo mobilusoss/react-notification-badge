3c558ea61a8b0987da68f6664a461405165d8188
Takeharu.Oshida <georgeosddev@gmail.com>
2016-08-20 21:59:44 +0900

    move index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-var NotificationBadge = require('./lib/components/NotificationBadge').default;
-
-NotificationBadge.Effect = require('./lib/components/Effect').default;
-module.exports = NotificationBadge;
